67dc52729aadb57571968d806aa58f085cdb0cfa
Shangxin <shangxin@outlook.com>
2016-04-24 21:26:18 +0800

    修正注册的JS

--- WebContent/WebPages/sources/script/person/register.js
+++ WebContent/WebPages/sources/script/person/register.js
@@ -2,7 +2,7 @@ function register(){
 	var registerForm = document.getElementById("registerForm");
 	var account = document.getElementById("username");
 	var pass = document.getElementById("password");
-	var confirmPass = document.getElementById("confirmPass");
+	var confirmPass = document.getElementById("confirmPassword");
 	var email = document.getElementById("email");
 	var nickname = document.getElementById("nickname");
 	var registerButton = document.getElementById("btnRegister");
@@ -14,7 +14,7 @@ function register(){
 					return 0;
 		}else{
 			confirmPass.setCustomValidity("");
-			return 0;
+			return 1;
 		}
 	}
 	function registerRequest(form,url,method){
@@ -23,25 +23,25 @@ function register(){
 	    lReq.send(form);
 	    return lReq.responseText;
 	}
-	
-	if(checkPassword()){//密码格式没写错的话开始发送请求
-		registerForm = new FormData(registerForm);
-	    //var username = document.getElementById("username").value;
-	    //var password = document.getElementById("password").value;
-	    //loginForm.append("name",username);
-	    //loginForm.append("pass",password);
-	    var result=registerRequest(loginForm,"http://localhost:8080/Erudite/register","POST");//发送注册请求
-	    
-	    debugger;
-	    var resObj=eval('['+result+']');
-	    if(typeof(resObj == 'object')){
-				if (resObj.suc==1 && resObj.err == 0) {//注册成功
-					console.log("register success");
-				}else console.log("register failed");
+	registerButton.onclick = function(){
+		if(checkPassword()){//密码格式没写错的话开始发送请求
+			registerForm = new FormData(registerForm);
+		    //var username = document.getElementById("username").value;
+		    //var password = document.getElementById("password").value;
+		    //loginForm.append("name",username);
+		    //loginForm.append("pass",password);
+		    var result=registerRequest(registerForm,"http://localhost:8080/Erudite/register","POST");//发送注册请求
+		    
+		    debugger;
+		    var resObj=eval('['+result+']');
+		    if(typeof(resObj == 'object')){
+					if (resObj.suc==1 && resObj.err == 0) {//注册成功
+						console.log("register success");
+					}else console.log("register failed");
+			}
 		}
 	}
-	
 
 }
-document.getElementById("btnRegister").addEventListener("click","register");
-debugger;
+//document.getElementById("btnRegister").addEventListener("click","register");
+register();
