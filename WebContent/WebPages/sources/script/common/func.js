1b66475b4df1dd9b1cbead8d4d1d4e7bd28fbf54
Shangxin <shangxin@outlook.com>
2016-05-06 15:40:10 +0800

    遍历文件功能1

---
+++ WebContent/WebPages/sources/script/common/func.js
@@ -0,0 +1,7 @@
+var detailedList = document.getElementByClassName("detailedList");
+for(var i=0;i<detailedList.length;i++){
+	detailedList[i].addEventListener("click",function(){
+		var typeName=this.typename;
+		
+	});
+}
