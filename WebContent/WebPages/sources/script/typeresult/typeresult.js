0fc799184ac8e7fc3efcc8976bfa6a8aa65f0b63
Shangxin <shangxin@outlook.com>
2016-06-14 09:46:36 +0800

    最新版

--- WebContent/WebPages/sources/script/typeresult/typeresult.js
+++ WebContent/WebPages/sources/script/typeresult/typeresult.js
@@ -1,44 +1,25 @@
 
-function downloadFile(url) { //下载文件函数
-    try{ 
-        var elemIF = document.createElement("iframe");   
-        elemIF.src = url;   
-        elemIF.style.display = "none";   
-        document.body.appendChild(elemIF);   
-    }catch(e){ 
-
-    } 
-}
-
-
 // 文件预览功能
-function viewFile(fileId,transitionPath) {// 未完成
-	window.location.href="http://localhost:8080/Erudite/WebPages/view.jsp?transitionPath=" + transitionPath;//跳不动
-	//console.log("跳转完毕");
+function viewFile(fileId, transitionPath) {// 未完成
+	window.location.href = "http://localhost:8080/Erudite/WebPages/view.jsp?transitionPath="
+			+ transitionPath;
+	// console.log("跳转完毕");
 }
 
-function dlFile(fileId) {// 未完成
-	// 获取文档接口getFile
-	var result = JSONReaquest("http://localhost:8080/Erudite/getFile?fileId="
-			+ fileId, "GET");
-	if (result == false) {
-		consolt.log("获取下载链接失败");
-		return false;
-	}
-	var fileURL = result.fileURL;
-	// 开始下载文件
-	downloadFile(fileURL);
+function dlFile(realPath) {// 未完成
+	
+	window.location.href = realPath ;// 跳不动
 }
 
 function getFile(rsEle) {
-	if (rsEle.transitionPath != "") { //有路径，代表能预览，执行预览
-		viewFile(rsEle.id,rsEle.transitionPath);
-	} else {//没有路径，不能预览，执行下载
-		dlFile(this.id);
+	if (rsEle.transitionPath != undefined) { // 有路径，代表能预览，执行预览
+		console.log("预览文件");
+		viewFile(rsEle.id, rsEle.transitionPath);
+	} else {// 没有路径，不能预览，执行下载
+		console.log("下载文件");
+		dlFile(this.realPath);
 	}
 }
-
-
 
 // 遍历文件功能
 
@@ -46,28 +27,31 @@ function getFile(rsEle) {
 function veiwFileListPage(id) {
 	var resObj = JSONRequest(
 			"http://localhost:8080/Erudite/GetFileListById?fdId=" + id, "GET");
+	var resultShow = "";
 	if (resObj == false) {
 		console.log("get faled");
-		return false;
-	}
-	var resultShow = "";
-	for (var i = 0; i < resObj.data.length; i++) {
-		console.log(resObj.data[i]);
-		var url = resObj.data[i].real_path;
-		var IconUrl = "";
-		var fileName = resObj.data[i].f_name;
-		var fileDescribe = resObj.data[i].descrp;
-		var id = resObj.data[i].f_id;
-		var transitionPath = "";
-		if ("html_path" in resObj.data[i])
-			transitionPath = resObj.data[i].html_path;
-		resultShow += "<div class='resultShow' transitionPath='"
-				+ transitionPath + "' id='" + id + "' onClick='getFile(this)'>"
-				+ // 绑定点击事件，查看文件详情
-				"<div class='fileIcon' ><img src='" + IconUrl + "'></div>"
-				+ "<div class='fileName'> " + fileName + " </div>"
-				+ "<div class='fileDescribe'> " + fileDescribe
-				+ " </div></div>";
+		resultShow = "加载失败";
+	} else {
+		if (resObj.data.length == 0)
+			resultShow = "无结果";
+		for (var i = 0; i < resObj.data.length; i++) {
+			console.log(resObj.data[i]);
+			var realPath = resObj.data[i].real_path;
+			var IconUrl = "";
+			var fileName = resObj.data[i].f_name;
+			var fileDescribe = resObj.data[i].descrp;
+			var id = resObj.data[i].f_id;
+			var transitionPath = "";
+			if ("html_path" in resObj.data[i])
+				transitionPath = resObj.data[i].html_path;
+			resultShow += "<div class='resultShow' realPath=" + realPath +
+					"transitionPath='" + transitionPath + "' id='" + id
+					+ "' onClick='getFile(this)'>" + // 绑定点击事件，查看文件详情
+					"<div class='fileIcon' ><img src='" + IconUrl + "'></div>"
+					+ "<div class='fileName'> " + fileName + " </div>"
+					+ "<div class='fileDescribe'> " + fileDescribe
+					+ " </div></div>";
+		}
 	}
 	console.log(resultShow);
 	document.getElementById("result").innerHTML = resultShow;
